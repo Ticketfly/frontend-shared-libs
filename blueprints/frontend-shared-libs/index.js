29e6c865eeee122abfaf5636afd7a2b8910c8d26
Danny Alvarez <danny@ticketfly.com>
2015-04-22 16:25:59 -0700

    Adding blueprint for node-uuid

---
+++ blueprints/frontend-shared-libs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  name: 'frontend-shared-libs',
+  
+  included: function(app) {
+      this._super.included(app);
+
+      app.import('bower_components/node-uuid/uuid.js');
+  },
+  
+  afterInstall: function() {
+    this.addBowerPackageToProject('node-uuid', '~1.4.3');
+  }
+};
