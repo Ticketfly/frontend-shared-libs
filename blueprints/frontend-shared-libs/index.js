a6b5fa761ffce42cff469488f3d47b810db31829
Danny Alvarez <danny@ticketfly.com>
2015-04-22 16:38:59 -0700

    Fixing indents

--- blueprints/frontend-shared-libs/index.js
+++ blueprints/frontend-shared-libs/index.js
@@ -4,9 +4,9 @@ module.exports = {
   name: 'frontend-shared-libs',
   
   included: function(app) {
-      this._super.included(app);
+    this._super.included(app);
 
-      app.import('bower_components/node-uuid/uuid.js');
+    app.import('bower_components/node-uuid/uuid.js');
   },
   
   afterInstall: function() {
