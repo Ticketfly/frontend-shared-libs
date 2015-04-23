e692d13db5465086d689f64ed17400c8dae5ceb9
Danny Alvarez <danny@ticketfly.com>
2015-04-22 17:00:34 -0700

    Moving the app.import to the index.js in root

--- blueprints/frontend-shared-libs/index.js
+++ blueprints/frontend-shared-libs/index.js
@@ -2,13 +2,7 @@
 
 module.exports = {
   name: 'frontend-shared-libs',
-  
-  included: function(app) {
-    this._super.included(app);
-
-    app.import('bower_components/node-uuid/uuid.js');
-  },
-  
+    
   afterInstall: function() {
     this.addBowerPackageToProject('node-uuid', '~1.4.3');
   }
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'frontend-shared-libs'
+  name: 'frontend-shared-libs',
+  
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/node-uuid/uuid.js');
+  }
 };
