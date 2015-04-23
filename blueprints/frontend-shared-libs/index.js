9687e0e83a91ab10262d5cddfc402daacd6338dc
Danny Alvarez <danny@ticketfly.com>
2015-04-23 14:50:33 -0700

    Adding normalizeEntityName function

--- blueprints/frontend-shared-libs/index.js
+++ blueprints/frontend-shared-libs/index.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   name: 'frontend-shared-libs',
+  
+  normalizeEntityName: function() {  },
     
   beforeInstall: function() {
     return this.addBowerPackageToProject('node-uuid', '~1.4.3');
